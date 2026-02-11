869ee8661d1fba0f8a5ee95e58c0d13bd939757a
Michael Kronthal <michaelkronthal@michaels-mbp.lan>
2026-02-11 12:21:31 -0800

    Add PWA support for install to home screen

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'weight-tracker-v1';
+const ASSETS = [
+  '/weight-tracker/',
+  '/weight-tracker/index.html',
+  '/weight-tracker/manifest.json',
+  '/weight-tracker/icon-192.png',
+  '/weight-tracker/icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
